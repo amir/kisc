c715220ac8d598427ff319c967d8a514b6819d41
Amir Saeid <amir@glgdgt.com>
2017-09-09 19:29:39 +0100

    Add the ability to add InitializerConfigurations

--- main.go
+++ main.go
@@ -6,17 +6,19 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/ghodss/yaml"
 	"github.com/julienschmidt/httprouter"
 	yaml_util "k8s.io/apimachinery/pkg/util/yaml"
 	core "k8s.io/client-go/pkg/api/v1"
-	"k8s.io/client-go/pkg/apis/apps/v1beta1"
+	ar_v1alpha1 "k8s.io/client-go/pkg/apis/admissionregistration/v1alpha1"
+	apps_v1beta1 "k8s.io/client-go/pkg/apis/apps/v1beta1"
 )
 
-type deploymentInitializer func(d *v1beta1.Deployment) error
+type deploymentInitializer func(d *apps_v1beta1.Deployment) error
 
-var initContainersInitializer = func(d *v1beta1.Deployment) error {
+var initContainersInitializer = func(d *apps_v1beta1.Deployment) error {
 	initContainer := core.Container{
 		Name:  "init",
 		Image: "init",
@@ -26,7 +28,7 @@ var initContainersInitializer = func(d *v1beta1.Deployment) error {
 	return nil
 }
 
-var volumeInitializer = func(d *v1beta1.Deployment) error {
+var volumeInitializer = func(d *apps_v1beta1.Deployment) error {
 	volume := core.Volume{
 		Name: "test",
 	}
@@ -35,7 +37,7 @@ var volumeInitializer = func(d *v1beta1.Deployment) error {
 	return nil
 }
 
-func removePendingInitializer(d *v1beta1.Deployment, name string) {
+func removePendingInitializer(d *apps_v1beta1.Deployment, name string) {
 	is := d.ObjectMeta.Initializers.Pending[:0]
 	for _, x := range d.ObjectMeta.Initializers.Pending {
 		if x.Name != name {
@@ -50,6 +52,11 @@ var registeredInitializers = map[string]deploymentInitializer{
 	"init-container.kisc.kubernetes.io": initContainersInitializer,
 }
 
+var registeredRules = struct {
+	sync.RWMutex
+	m map[string][]ar_v1alpha1.Rule
+}{m: make(map[string][]ar_v1alpha1.Rule)}
+
 func Evaluate(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	b, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -57,7 +64,7 @@ func Evaluate(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		return
 	}
 	j, err := yaml_util.ToJSON(b)
-	var deployment v1beta1.Deployment
+	var deployment apps_v1beta1.Deployment
 	if err != nil {
 		http.Error(w, fmt.Sprintf("%s", err), http.StatusBadRequest)
 		return
@@ -89,9 +96,39 @@ func Evaluate(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprintf(w, "%s", out)
 }
 
+func Register(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("%s", err), http.StatusBadRequest)
+		return
+	}
+	j, err := yaml_util.ToJSON(b)
+	var configuration ar_v1alpha1.InitializerConfiguration
+	if err != nil {
+		http.Error(w, fmt.Sprintf("%s", err), http.StatusBadRequest)
+		return
+	}
+	err = json.Unmarshal(j, &configuration)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("%s", err), http.StatusBadRequest)
+		return
+	}
+
+	for _, i := range configuration.Initializers {
+		for _, r := range i.Rules {
+			registeredRules.Lock()
+			registeredRules.m[i.Name] = append(registeredRules.m[i.Name], r)
+			registeredRules.Unlock()
+		}
+	}
+
+	fmt.Fprintf(w, "%+v\n", registeredRules.m)
+}
+
 func main() {
 	router := httprouter.New()
 	router.POST("/evaluate", Evaluate)
+	router.POST("/register", Register)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
